41cdeaf1f12b1f25dafda63c9960f1cd5be0e2f1
Menencia <menencia@gmail.com>
2015-08-21 01:09:15 +0200

    feat(abilities): select/unselect ability by character

--- js/abilities.js
+++ js/abilities.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @param $http
+ * @param $scope
+ * @constructor
+ */
 function AbilitiesController($http, $scope) {
 
     var self = this;
@@ -9,21 +15,27 @@ function AbilitiesController($http, $scope) {
     self.$http = $http;
     self.$scope = $scope;
 
-    self.abilities = [];
+    self.data = {
+        "abilities" : [],
+        "characters": []
+    };
+
     $http.get('/data/abilities.json').
         then(function (r) {
-            self.abilities = r.data;
+            self.data.abilities = r.data;
             self.autocomplete(r.data);
         });
 
-    self.characters = [];
     $http.get('/data/characters.json').
         then(function (r) {
-            self.characters = r.data;
+            self.data.characters = r.data;
         });
 
-    self.selectedAbilities = [];
-    self.selectedCharacters = [];
+    // selected abilities from input
+    self.abilities = [];
+
+    // selected characters by abilities
+    self.characters = [];
 
 }
 
@@ -64,50 +76,137 @@ AbilitiesController.prototype.autocomplete = function (data) {
 
 AbilitiesController.prototype.addAbility = function (value) {
 
-    // search for ability
-    var ability = _.find(this.abilities, {name: value});
+    // stop if it's already in abilities
+    var b = _.find(this.abilities, {name: value});
+    if (b) throw new Error('Ability already added!');
 
-    // if any
-    if (ability) {
-        this.selectedAbilities.push(ability);
-        for (var i in this.characters) {
-            var character = this.characters[i];
-            if (character.abilities[ability.type] && ability.rarity <= character.abilities[ability.type]) {
-                if (character.choices) {
-                    character.choices.push(ability);
-                } else {
-                    character.choices = [ability];
-                }
-                this.selectedCharacters.push(character);
+    // search ability data
+    var a = _.find(this.data.abilities, {name: value});
+    if (!a) throw new Error('Ability not found!');
+
+    var ability = new Ability(this, a);
+    this.abilities.push(ability);
+
+    for (var i in this.data.characters) {
+        var c = this.data.characters[i];
+        if (c.abilities[a.type] && a.rarity <= c.abilities[a.type]) {
+            var character = _.find(this.characters, {name: c.name});
+
+            // add character if not
+            if (!character) {
+                character = new Character(this, c);
+                this.characters.push(character);
             }
+
+            character.addLinkedAbility(ability);
+
         }
     }
 };
-
 
 AbilitiesController.prototype.removeAbility = function (ability) {
 
-    var removed;
+    _.remove(this.abilities, {name: ability.name});
 
-    removed = _.remove(this.selectedAbilities, ability);
-
-    if (removed.length > 0) {
-        for (var i in this.characters) {
-            var character = this.characters[i];
-
-            if (!character.choices) continue;
-
-            removed = _.remove(character.choices, ability);
-            if (character.choices.length == 0) {
-                _.remove(this.selectedCharacters, character);
-            }
-        }
+    for (var i in this.characters) {
+        var character = this.characters[i];
+        _.remove(character.abilities, {name: ability.name});
     }
 
+    this.cleanCharacters();
+
 };
 
-AbilitiesController.prototype.getAbilities = function (character) {
-    var res = [];
+AbilitiesController.prototype.cleanCharacters = function () {
 
-    return res;
+    _.remove(this.characters, function (c) {
+        return (c.abilities.length == 0 && c.selected.length == 0);
+    });
+
 };
+
+
+/**
+ *
+ * @constructor
+ */
+function Character(ctrl, data) {
+
+    // controller instance
+    this.ctrl = ctrl;
+
+    // shortcut
+    this.name = data.name;
+
+    // data from json
+    this.data = data;
+
+    // suggested abilities
+    this.abilities = [];
+
+    // character can have many abilities
+    // but they can select 2 max
+    this.selected = [];
+}
+
+/**
+ * Add the ability for the character
+ * @param {Ability} a
+ */
+Character.prototype.addLinkedAbility = function (a) {
+    this.abilities.push(
+        new LinkedAbility(this, a)
+    );
+};
+
+/**
+ *
+ * @param character
+ * @param ability
+ * @constructor
+ */
+function LinkedAbility(character, ability) {
+    this.character = character;
+
+    // extends ability
+    this.name = ability.name;
+    this.data = ability.data;
+}
+
+/**
+ * Deplace from abilities to selected
+ */
+LinkedAbility.prototype.select = function () {
+    _.remove(this.character.abilities, {name: this.name});
+    this.character.selected.push(this);
+    this.character.ctrl.removeAbility(this);
+};
+
+/**
+ * Remove from selected
+ */
+LinkedAbility.prototype.unselect = function () {
+    _.remove(this.character.selected, this);
+
+    this.character.ctrl.cleanCharacters([this.character.name]);
+};
+
+/**
+ * Select the LinkedAbility
+ */
+LinkedAbility.prototype.canBeSelected = function () {
+    return (this.character.selected.length < 2);
+};
+
+/**
+ *
+ * @param ctrl
+ * @param data
+ * @constructor
+ */
+function Ability(ctrl, data) {
+    this.ctrl = ctrl;
+
+    this.name = data.name;
+    this.data = data;
+}

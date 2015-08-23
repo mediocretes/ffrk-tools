51fd5b1380fe5887a91e84d41b3b6945d885f069
Menencia <menencia@gmail.com>
2015-08-23 16:35:28 +0200

    fix(general): change home page

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ app.config(function ($translateProvider, $routeProvider) {
             controller : 'AbilitiesController as app'
         }).
         otherwise({
-            redirectTo: '/abilities'
+            redirectTo: '/inventory'
         });
 });
 
@@ -26,28 +26,3 @@ app.service('Main', ['$http', '$translate', Main]);
 app.controller('MenuController', ['Main', '$location', MenuController]);
 app.controller('InventoryController', ['Main', '$scope', '$sce', InventoryController]);
 app.controller('AbilitiesController', ['Main', '$scope', AbilitiesController]);
-
-function eventCancel(e) {
-    if (!e)
-        if (window.event) e = window.event;
-        else return;
-    if (e.cancelBubble != null) e.cancelBubble = true;
-    if (e.stopPropagation) e.stopPropagation();
-    if (e.preventDefault) e.preventDefault();
-    if (window.event) e.returnValue = false;
-    if (e.cancel != null) e.cancel = true;
-}
-
-app.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if (event.which === 13) {
-                scope.$apply(function () {
-                    scope.$eval(attrs.ngEnter);
-                });
-
-                event.preventDefault();
-            }
-        });
-    };
-});

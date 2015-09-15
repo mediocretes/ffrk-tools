356d9ab98da45b0e40276a3b603b19b3e91840b1
Menencia <menencia@gmail.com>
2015-09-16 00:01:21 +0200

    fix(cron): refresh cron tab syntax

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Refresh = require('./refresh.js');
 
 var CronJob = require('cron').CronJob;
 var job = new CronJob({
-    cronTime: '* 0 3 * * *',
+    cronTime: '0 0 3 1/1 * ? *',
     onTick: function() {
         Refresh();
     },
